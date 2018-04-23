e01e5532af7eef5f6ef666fb798fbb68e3dbf47b
Jacob Percival <jnperciv@gmail.com>
2018-04-22 19:28:19 -0600

    navbar routes to anchors, signin opens UserAuthLightbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import axios from 'axios';
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Grid, Row } from 'react-bootstrap'
 import 'bootstrap-css';
 import './App.css';
 
+import Navbar from './Navbar/Navbar';
+
 class App extends Component {
   state = {
-    data: ""
+    data: "",
+    homeY: 0,
+    aboutY: 0,
+    contactY: 0
   };
 
   componentDidMount(){
+    const home = ReactDOM.findDOMNode(this.refs.home);
+    const about = ReactDOM.findDOMNode(this.refs.about);
+    const contact = ReactDOM.findDOMNode(this.refs.contact);
+    console.log(home.offsetTop);
+    this.setState({homeY: 157});
+    this.setState({aboutY: about.offsetTop});
+    this.setState({contactY: contact.offsetTop});
+    console.log(this.state);
     axios.get("http://localhost:3001")
       .then(res => {
         this.setState({data: res.data});
@@ -20,19 +34,16 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
         <Grid className="show-grid">
           <Row >
-            <Col xs={2}>
-            </Col >
-            <Col className="App" xs={8}>
-              {this.state.data}
-            </Col>
-            <Col xs={2}>
-            </Col >
+            <Navbar {...this.state}/>
+          </Row>
+          <Row>
+            <div id="home" ref="home" className="tall">Home</div>
+            <div id="about" ref="about" className="tall">About</div>
+            <div id="contact" ref="contact" className="tall">Contact</div>
           </Row>
         </Grid>
-      </div>
     );
   }
 }
